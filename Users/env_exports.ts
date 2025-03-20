014a0d9ec33f899aee38e2fef7732dbd06be0869
Nitai <nitaidaud@gmail.com>
2025-03-20 10:02:46 +0200

    Init

---
+++ Users/env_exports.ts
@@ -0,0 +1,1 @@
+export const { DATABASE_URL, PORT, JWT_KEY } = process.env;
--- Users/src/index.ts
+++ Users/src/index.ts
@@ -1,7 +1,13 @@
 import dotenv from "dotenv";
+import { DATABASE_URL, JWT_KEY } from "../env_exports";
 
-dotenv.config()
+dotenv.config();
 
-const start = async () => { 
-    
-}
+const start = async () => {
+  if (!DATABASE_URL) {
+    throw new Error("Missing db url");
+  }
+  if (!JWT_KEY) {
+    throw new Error("Missing jwt key");
+  }
+};

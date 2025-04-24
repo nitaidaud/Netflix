7f48052dcf214f555560531b556c77579af3ace1
Nikolatesla20000 <avielrez14@gmail.com>
2025-04-24 16:32:09 +0300

    feat: add dynamic category rendering in Home component for improved movie browsing

--- Client/Netflix/src/pages/Home.tsx
+++ Client/Netflix/src/pages/Home.tsx
@@ -1,3 +1,4 @@
+
 import HeroSection from "@/components/home/HeroSection";
 import CategoryCarousel from "@/components/home/CategoryCarousel";
 import { useHomeContent } from "@/hooks/useHomeContent";
@@ -16,6 +17,16 @@ const Home = () => {
   }
 
   const heroMovie = data.newMovies[10];
+  const categories = [
+    { key: "newMovies", title: "New Releases", link: "/browse?category=new" },
+    { key: "comedy", title: "Comedy Movies", link: "/browse?category=comedy" },
+    { key: "horror", title: "Horror Movies", link: "/browse?category=horror" },
+    { key: "action", title: "Action Movies", link: "/browse?category=action" },
+    { key: "romance", title: "Romance Movies", link: "/browse?category=romance" },
+    { key: "kids", title: "Kids Movies", link: "/browse?category=kids" },
+    { key: "documentary", title: "Documentaries", link: "/browse?category=documentary" },
+  ];
+  console.log(data);
   return (
     <div className="w-full ">
       <HeroSection
@@ -24,24 +35,16 @@ const Home = () => {
         backdropPath={heroMovie.backdrop_path ?? ""}
       />
 
-      <div className="space-y-16 px-6 lg:px-12 py-10 bg-gradient-to-t from-zinc-900 via-zinc-900/90 to-transparent -mt-13 relative z-20">
-        <CategoryCarousel
-          title="Action Movies"
-          movies={data.action}
-          categoryLink="/browse?category=action"
-        />
-        <CategoryCarousel
-          title="Comedy Movies"
-          movies={data.comedy}
-          categoryLink="/browse?category=comedy"
-        />
-        <CategoryCarousel
-          title="Horror Movies"
-          movies={data.horror}
-          categoryLink="/browse?category=horror"
-        />
-        {/*  add more  */}
-      </div>
+<div className="space-y-16 px-6 lg:px-12 py-10 bg-gradient-to-t from-black via-black/90 to-transparent -mt-13 relative z-20">
+  {categories.map(({ key, title, link }) => (
+    <CategoryCarousel
+      key={key}
+      title={title}
+      movies={data[key as keyof typeof data]} 
+      categoryLink={link}
+    />
+  ))}
+</div>
     </div>
   );
 };
